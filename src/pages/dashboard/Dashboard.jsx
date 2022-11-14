57172526925f292aa739eb1965d818763eac942f
catgoesmeow14 <exoexo1990@gmail.com>
2022-11-14 14:15:33 +0800

    fix route, add transition, and add delay

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -13,26 +13,33 @@ const Dashboard = () => {
 
   const navigation = useNavigate();
 
-  const navigateChat = () => {
-    navigation('chat');
+  const navigateChat = async () => {
+    await delay(2000);
+    navigation('/chat');
     console.log('Masuk ke Chat sama Bot');
   };
 
+  const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
   return (
     <div className="w-screen h-screen bg-gray-300 flex flex-col items-center">
-      <div className="w-[70%] h-auto p-10 bg-white m-auto content-center px-20 drop-shadow-xl rounded-md">
+      <div className=" w-[70%] h-auto p-10 bg-white m-auto content-center px-20 drop-shadow-xl rounded-md">
         <h1 className="text-6xl text-center capitalize mb-10">
           Hi,{' '}
-          <a className="font-bold text-indigo-800 hover:text-sky-500">
+          <span
+            className="transition-all delay-150 font-bold
+           text-indigo-800 hover:text-sky-500 hover:text-8xl duration-400">
             {user.username}
-          </a>
+          </span>
           ! Welcome bro😎
         </h1>
 
         <div className="flex justify-center">
           <button
-            // className="btn bg-white max-w-sm text-gray-800 hover:btn"
-            className="mt-7 btn sm:btn-sm md:btn-md lg:btn-lg text-white glass bg-blue-600 hover:bg-blue-700 active:bg-blue-800 focus:outline-none focus:ring focus:ring-blue-300"
+            className="mt-7 transition ease-in-out delay-150 btn sm:btn-sm md:btn-md lg:btn-lg 
+            text-white glass bg-blue-600 hover:-translate-y-2 
+            hover:scale-110 transform-gpu hover:bg-blue-600 duration-400
+            active:bg-blue-800 focus:outline-none focus:ring focus:ring-blue-300"
             type="button"
             onClick={navigateChat}>
             Yuk Curhat!
