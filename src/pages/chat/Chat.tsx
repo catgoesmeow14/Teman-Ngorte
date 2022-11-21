ee9931416e87410e947ffb3fc26b8e5c3dfaf842
catgoesmeow14 <exoexo1990@gmail.com>
2022-11-21 14:00:23 +0800

    fix layout chat and bubblechat

--- src/pages/chat/Chat.tsx
+++ src/pages/chat/Chat.tsx
@@ -1,27 +1,47 @@
 import React from 'react';
 import BubbleChat from '../../components/BubbleChat';
 import useFetch from '../../hooks/useFetch';
+import { IoArrowBack, IoSend } from 'react-icons/io5';
+import { useNavigate } from 'react-router-dom';
 
 // tes get method
 const Chat = () => {
   const chatList = useFetch('https://chatbot-api-three.herokuapp.com/chat');
   console.log(chatList);
+  const ButtonStyle = { margin: '0px 7px' };
+  // const iconStyles = { color: 'white' };
 
   const user = JSON.parse(localStorage.getItem('user') || '');
+
+  const navigation = useNavigate();
+
+  const navigateDashboard = async () => {
+    navigation('/');
+    console.log('Balik ke dashboard');
+  };
 
   return (
     <div className="w-screen h-screen bg-gray-200 flex flex-col items-center">
       <div className="bg-white m-auto h-[70%] w-[40%] flex flex-col drop-shadow-xl rounded-lg">
-        <div className="bg-primary py-3 px-7 w-full flex flex-row items-center space-x-4 rounded-t-lg">
+        <div className="bg-primary py-3 px-3 w-full flex flex-row items-center space-x-4 rounded-t-lg">
+          <button
+            type="button"
+            // style={ButtonStyle}
+            className="btn btn-circle btn-primary"
+            onClick={navigateDashboard}
+          >
+            <IoArrowBack style={{ color: 'white', fontSize: '30px' }} />
+          </button>
+
           <div className="avatar">
-            <div className=" w-[80px] rounded-full">
+            <div className=" sm:w-[40px] md:w-[60px] lg:w-[80px] rounded-full">
               <img
                 src="https://i.pinimg.com/564x/8c/b6/1f/8cb61f5c432e146e4b9aad26cc5effbb.jpg"
                 alt="avatar"
               />
             </div>
           </div>
-          <h1 className="font-bold text-4xl text-white capitalize">
+          <h1 className="font-bold sm:text-2xl md:text-3xl lg:text-4xl text-white capitalize">
             TimpalBot
           </h1>
         </div>
@@ -42,10 +62,14 @@ const Chat = () => {
           <input
             type="text"
             placeholder="Type your message..."
-            className="input input-bordered w-full text-2xl"
+            className="input input-bordered w-full sm:text-md md:text-xl lg:text-2xl"
           />
-          <button type="button" className="btn btn-primary btn-circle">
-            send
+          <button
+            type="button"
+            style={ButtonStyle}
+            className="btn btn-circle btn-primary btn-md"
+          >
+            <IoSend style={{ color: 'white', fontSize: '25px' }} />
           </button>
         </form>
       </div>
